588dc3f1a6e6ed45ea9e12758b65339dc294f6fe
abhay <abhaytiwari4281@gmail.com>
2024-03-14 10:42:46 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +19,18 @@ class MyApp extends StatelessWidget {
    
 
     return MaterialApp(
-      home: HomePage(),
+      
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: { // routes likha h to home: homePage( ) ye htana pdhega verna chlega nhi
+        "/":(context) => LoginPage(),
+        "/home":(context) => HomePage(),
+        "/login":(context) => LoginPage()
+      },
     );
   }
+
+  
 }
